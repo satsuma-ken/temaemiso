c46977d1328ff752e10f76ce5dd1e9761fda1916
satsuma-ken <sakurajima.ken@gmail.com>
2024-04-24 01:10:05 +0900

    【検討】HTML変換・保管手法

--- temaemiso/blog/views.py
+++ temaemiso/blog/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.template import loader, Template
 from temaemiso.blog.models import Article
 
+# 記事が見つからなかった時のとりあえず表示する記事ID
+ERROR_ARTICLE_ID = 1
+
 
 def index(request):
     template = loader.get_template("blog_home.html")  # テンプレートをロードする
@@ -17,15 +20,11 @@ def index(request):
 def article(request, article_id: int = 1):
     template = loader.get_template("blog_articles.html")  # テンプレートをロードする
     disp_article = search_article_by_id(article_id=article_id)
-    # print(disp_article.article_title)
     disp_article_title = disp_article.article_title
-    # print(disp_article.article_overview)
-    # print(disp_article.content_set.values_list("content", flat=True))
     disp_content_string = disp_article.content_set.values_list("content", flat=True)[0]
-    # disp_content_html = Template(disp_content_string)
     context = {
         "blog_title": "手前みそブログ",
-        "blog_content_title": f"「手前味噌ですが。。。**{disp_article_title}** です。。。」",
+        "blog_content_title": disp_article_title,
         "blog_content": disp_content_string,
         "topic_disp": False,
         "test_flag": True,
@@ -39,4 +38,7 @@ def search_article_by_id(article_id: int = 1):
         article = Article.objects.get(pk=article_id)
         return article
     except Article.DoesNotExist:
-        print("指定したIDの記事が見つかりませんでした。")
+        print("記事が見つかりませんでした。とりあえずの記事を表示します。")
+        # ひとまずID=1の記事を返す
+        article = Article.objects.get(pk=ERROR_ARTICLE_ID)
+        return article
