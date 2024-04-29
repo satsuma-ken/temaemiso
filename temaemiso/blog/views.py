45b723fd82ed54fba8ba705f58989e62097001fb
satsuma-ken <sakurajima.ken@gmail.com>
2024-04-29 17:44:46 +0900

    【検討】HTML変換・保管手法決定

--- temaemiso/blog/views.py
+++ temaemiso/blog/views.py
@@ -7,6 +7,9 @@ ERROR_ARTICLE_ID = 1
 
 
 def index(request):
+    articles = search_article_by_new()
+    for article in articles:
+        print(article)
     template = loader.get_template("blog_home.html")  # テンプレートをロードする
     context = {
         "blog_title": "手前みそブログ",
@@ -27,6 +30,21 @@ def article(request, article_id: int = 1):
         "blog_content_title": disp_article_title,
         "blog_content": disp_content_string,
         "topic_disp": False,
+        "test_flag": False,
+    }  # テンプレートに当てはめる値
+    # HTTPレスポンスとして返却
+    return HttpResponse(template.render(context, request))
+
+
+def article_test(request):
+    template = loader.get_template("blog_articles.html")  # テンプレートをロードする
+    disp_article_title = "test表示です"
+    disp_content_string = "test表示です"
+    context = {
+        "blog_title": "手前みそブログ",
+        "blog_content_title": disp_article_title,
+        "blog_content": disp_content_string,
+        "topic_disp": False,
         "test_flag": True,
     }  # テンプレートに当てはめる値
     # HTTPレスポンスとして返却
@@ -42,3 +60,14 @@ def search_article_by_id(article_id: int = 1):
         # ひとまずID=1の記事を返す
         article = Article.objects.get(pk=ERROR_ARTICLE_ID)
         return article
+
+
+def search_article_by_new():
+    try:
+        articles = Article.objects.get_latest_three_articles()
+        return articles
+    except Article.DoesNotExist:
+        print("記事が見つかりませんでした。とりあえずの記事を表示します。")
+        # ひとまずID=1の記事を返す
+        article = Article.objects.get(pk=ERROR_ARTICLE_ID)
+        return articles
